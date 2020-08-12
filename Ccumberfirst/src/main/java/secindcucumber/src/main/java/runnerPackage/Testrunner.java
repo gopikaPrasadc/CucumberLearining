4cf110b8e9ce83fb74e3123abf83978a8043b388
gopikaPrasadc <65433181+gopikaPrasadc@users.noreply.github.com>
2020-08-13 02:46:35 +0530

    tags included

--- Ccumberfirst/src/main/java/secindcucumber/src/main/java/runnerPackage/Testrunner.java
+++ Ccumberfirst/src/main/java/secindcucumber/src/main/java/runnerPackage/Testrunner.java
@@ -7,12 +7,15 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		features="D:\\Testing\\eclipse\\secindcucumber\\src\\main\\java\\feature\\demotours.feature",
+		features="D:\\Testing\\eclipse\\secindcucumber\\src\\main\\java\\feature\\tags.feature",
 		glue= {"stepDefinition"},
 		dryRun=false,
 		//strict = true, //checks the mapping between feature and stepdefinitions
 		monochrome = true, // to make readable output in console
-		format = {"pretty","html:test-outout"})
+		format = {"pretty","html:test-outout"},
+		//tags= {"@smokeTest"}
+		tags={"@InvalidTest "}
+		)
 		
 public class Testrunner {
 
